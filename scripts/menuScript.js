18131de7f7da01263b61085d9d1fd51c65370cb1
Bruno Siqueira <santoserobinho@gmail.com>
2024-02-10 23:46:15 -0300

    05 Assignment: Chamber Home Page Continued

--- scripts/menuScript.js
+++ scripts/menuScript.js
@@ -5,3 +5,17 @@ hamButton.addEventListener("click", () => {
 	navigation.classList.toggle("open");
 	hamButton.classList.toggle("open");
 });
+
+const darkModeToggle = document.getElementById('darkModeToggle');
+
+function toggleDarkMode() {
+  if (darkModeToggle.checked) {
+    document.body.classList.add('dark-theme');
+  } else {
+    document.body.classList.remove('dark-theme');
+  }
+}
+
+darkModeToggle.addEventListener('change', toggleDarkMode);
+
+toggleDarkMode();
