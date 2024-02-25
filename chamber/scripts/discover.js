5ed3e089158b3ecd778b9797cef1d255fdc88c10
Bruno Siqueira <santoserobinho@gmail.com>
2024-02-25 02:06:22 -0300

    update

--- chamber/scripts/discover.js
+++ chamber/scripts/discover.js
@@ -22,3 +22,27 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   }
 });
+
+
+document.addEventListener("DOMContentLoaded", function() {
+  let lastVisit = localStorage.getItem("lastVisit");
+  let now = Date.now();
+  let daysSinceLastVisit = Math.floor((now - lastVisit) / (1000 *  60 *  60 *  24));
+
+  let visitMessage;
+  if (lastVisit) {
+    if (daysSinceLastVisit <  1) {
+      visitMessage = "Back so soon! Awesome!";
+    } else {
+      visitMessage = `You last visited ${daysSinceLastVisit} ${daysSinceLastVisit ===  1 ? "day" : "days"} ago.`;
+    }
+  } else {
+    visitMessage = "Welcome! Let us know if you have any questions.";
+  }
+
+  // Display the visit message in the new leftCard
+  document.getElementById("visitMessage").textContent = visitMessage;
+
+  // Update the last visit date in LocalStorage
+  localStorage.setItem("lastVisit", now);
+});
