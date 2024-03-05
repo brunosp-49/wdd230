520cc5195c529edfb34933b658f8dbd9d95999d5
Bruno Siqueira <santoserobinho@gmail.com>
2024-03-05 19:28:23 -0300

    update

--- scripts/links.js
+++ scripts/links.js
@@ -1,25 +1,15 @@
-const baseURL = "https://yourgithubusername.github.io/wdd230/";
-const linksURL = "https://yourgithubusername.github.io/wdd230/data/links.json";
-
-async function getLinks() {
- const response = await fetch(linksURL);
- const data = await response.json();
- displayLinks(data.lessons);
-}
-
 function displayLinks(weeks) {
- const linksContainer = document.getElementById('dynamic-links');
- weeks.forEach(week => {
-    const weekDiv = document.createElement('div');
-    weekDiv.innerHTML = `<h3>Week ${week.lesson}</h3>`;
-    week.links.forEach(link => {
-      const linkElement = document.createElement('a');
-      linkElement.href = baseURL + link.url;
-      linkElement.textContent = link.title;
-      weekDiv.appendChild(linkElement);
+    const activitiesContainer = document.getElementById('dynamic-learning-activities');
+    weeks.forEach(week => {
+       const weekDiv = document.createElement('div');
+       weekDiv.innerHTML = `<h3>Week ${week.lesson}</h3>`;
+       week.links.forEach(link => {
+         const linkElement = document.createElement('a');
+         linkElement.href = baseURL + link.url;
+         linkElement.textContent = link.title;
+         weekDiv.appendChild(linkElement);
+       });
+       activitiesContainer.appendChild(weekDiv);
     });
-    linksContainer.appendChild(weekDiv);
- });
-}
-
-getLinks();
+   }
+   
